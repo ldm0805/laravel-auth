067f215fda7f830924959d84332fd6ef77771c97
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-02-28 18:29:01 +0100

    card inserite

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.admin')
 @section('content')
-    Lista dei post
+<div id="index">
+    @foreach($projects as $project)
+        <section class="card">
+            <article class="containerImagenCard">
+                <img src="https://cdn.pixabay.com/photo/2018/02/07/14/27/pension-3137209_640.jpg" alt="">
+            </article>
+            <article class="containerDescriptionCard">
+                <p class="titleCard">{{$project->id}}. {{$project->title}}</p>
+                <p class="tecnologiesCard">{{$project->slug}}</p>
+                <p class="descriptionCard">{{$project->content}}</p>
+            </article>
+        </section>
+    @endforeach
+</div>
 @endsection
