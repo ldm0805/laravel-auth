bc413712d5e72d076bf468c140f2f88e39c5795f
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-01 10:07:32 +0100

    Cambio route della dashboard in app.blade

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -17,7 +17,10 @@ class ProjectController extends Controller
      */
     public function index()
     {
+        //Recupero tutti i dati dal database
         $projects = Project::all();
+
+        //Rimando alla view
         return view('admin.projects.index', compact('projects'));
     }
 
@@ -39,14 +42,22 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
+        // Ottengo i dati validati dalla richiesta
         $form_data = $request->validated();
-        dd($request->data_project);
+    
+        // Genero uno slug tramite una funzione (project.php) dal titolo del progetto
         $slug = Project::generateSlug($request->title);
+    
+        // Lo slug viene aggiunto ai dati del form
         $form_data['slug'] = $slug;
+    
+        // Creo un nuovo progetto nel database utilizzando i dati del form
         $newProj = Project::create($form_data);
-
-        return redirect()->route('admin.projects.index')->with('message', 'Post creato correttamente');
+    
+        // Reindirizzamento all'index con messaggio di conferma crezione
+        return redirect()->route('admin.projects.index')->with('message', 'Project creato correttamente');
     }
+    
 
     /**
      * Display the specified resource.
@@ -90,8 +101,11 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        //Elimino il project dal database;
         $project->delete();
-        return redirect()->route('admin.projects.index')->with('messagedelete', 'Post eliminato correttamente');
+
+        // Reindirizzamento all'index con messaggio di conferma eliminazione
+        return redirect()->route('admin.projects.index')->with('messagedelete', 'Project eliminato correttamente');
 
     }
 }
