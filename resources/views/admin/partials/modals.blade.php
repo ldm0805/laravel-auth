c298cbd214547e1da6b4ec97bf2cbf2b4494aa18
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-02 09:27:05 +0100

    cambio colore modale; navbar padding aumentato

--- resources/views/admin/partials/modals.blade.php
+++ resources/views/admin/partials/modals.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="delete-project-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
+      <div class="modal-content  bg-dark">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">ATTENZIONE</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
